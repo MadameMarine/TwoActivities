cab0974c22c5fbe29903f241f8faae64e7c08558
m.lesigne <marine.lesigne974@gmail.com>
2019-07-29 21:50:46 +0200

    All codelabs Done

--- app/src/androidTest/java/com/example/android/twoactivities/ActivityInputOutputTest.java
+++ app/src/androidTest/java/com/example/android/twoactivities/ActivityInputOutputTest.java
@@ -27,9 +27,11 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -64,5 +66,12 @@ public class ActivityInputOutputTest {
         onView(withId(R.id.text_header)).check(matches(isDisplayed()));
         onView(withId(R.id.button_second)).perform(click());
         onView(withId(R.id.text_header_reply)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void textInputOutput(){
+        onView(withId(R.id.editText_main)).perform(typeText("This is my Test after all this time"));
+        onView(withId(R.id.button_main)).perform(click());
+        onView(withId(R.id.text_message)).check(matches(withText("This is my Test after all this time")));
     }
 }
